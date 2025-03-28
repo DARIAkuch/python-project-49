ac96d5c6113bd63d68a26c152199d0f4b5df45e2
Daria <d.kuchkina2000@gmail.com>
2025-03-28 21:49:00 +0300

    Pre restructure

---
+++ brain_games/engine/game_engine.py
@@ -0,0 +1,22 @@
+from brain_games.engine.utils import (
+    welcome_user, ask_question, compare_answer, ROUND_LIMIT
+)
+
+def run_game(logic, rule, name):
+    name = welcome_user()
+    print(rule)
+    round_counter = 0
+    while round_counter < ROUND_LIMIT:
+        question, correct_answer = logic()
+        answer = ask_question(f'Question: {question}')
+        success = compare_answer(answer, correct_answer)
+        if success:
+            print("Correct!")
+            round_counter += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+        print(f"Congratulations, {name}!")
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,8 +4,16 @@ from brain_games.cli import welcome_user
 
 import prompt
 
+from brain_games.engine.game_engine import run_game
+
 def is_even(digit):
     return digit % 2
+
+def generate_question_and_answer():
+    number = random.randint(1, 100)
+    question = str(number)
+    correct_answer = "no" if is_even(number) else "yes"
+    return question, correct_answer
 
 
 def brain_even():
@@ -14,24 +22,21 @@ def brain_even():
     print(rule)
     counter = 0
     while counter < 3:
-        digit = random.randint(1, 100)
-        right_answer = is_even(digit)
-        print(f"Question: {digit}")
+        question, answer = generate_question_and_answer()
+        print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
-        if user_answer == 'yes':
-            user_answer = False
-        else:
-            user_answer = True
-        if right_answer == user_answer:
+        if answer == user_answer:
             print('Correct!')
             counter = counter + 1
-        elif right_answer != user_answer:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'. "
+        elif answer != user_answer:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'. "
                   f"Let's try again, {name}")
             return
+    print(f'Congratulations, {name}')
 
 def main():
-    brain_even()
+    game_rule = 'Answer "yes" if the number is even, otherwise answer "no".'
+    run_game(generate_question_and_answer, game_rule, "Even Game")
 
 if __name__ == '__main__':
     main()
